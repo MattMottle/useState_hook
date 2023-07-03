1b9683b4c95589952021d52e1057662ee54508c7
Matt Mottle <mattmottle1@gmail.com>
2023-07-03 11:58:15 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
-function App() {
+
+const StateTutorial = () => {
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState("Matt");
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  let onChange = (event) => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {counter}
+      </div>
+      <div>
+        <button onClick={increment}>Increment</button>
+      </div>
+      <div>
+        <input placeholder="type something..." onChange={onChange}/>
+      </div>
+      <div>
+        {inputValue}
+      </div>
+
     </div>
   );
 }
 
-export default App;
+export default StateTutorial;
